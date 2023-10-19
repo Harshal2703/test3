76e3002ad7d02ce5aa20ae9d22d9843ca3023488
Harshal2703 <harshalchaudhari2703@gmail.com>
2023-10-19 10:53:21 +0530

    1.1

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const http = require('http')
 const app = express()
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.DB_URI);
+const db = client.db('ChessMateMain');
+let collection = db.collection('Games');
+const io = new Server(server)
 const port = process.env.PORT || 3001
+io.on('connection', (socket) => {
+    socket.emit('ack', { 'connected': socket.id })
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
